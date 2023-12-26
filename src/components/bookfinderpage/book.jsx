6eb2e762f2f4c2eb960de0fa774d38a5586632c2
Gowtham1802 <gowthama7575@gmail.com>
2023-12-26 13:54:29 +0530

    add update codes

--- src/components/bookfinderpage/book.jsx
+++ src/components/bookfinderpage/book.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./book.css";
 import { FaSearch } from "react-icons/fa";
 import CardPage from "../cardpage/card";
+import axios from "axios";
 
 const BookFinder = () => {
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
+  const searchBook = (evt) => {
+    if (evt.key === "Enter") {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=" +
+            search +
+            "&key=AIzaSyBVeHQeoGrqwb1gdQhpvvWlgSE2nrZBJ34"
+        )
+        .then((res) => setData(res.data.items))
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <>
       <div className="header">
@@ -16,7 +31,13 @@ const BookFinder = () => {
         <div className="head-row2">
           <h2> Find Your Books </h2>
           <div className="search">
-            <input type="text" placeholder="Enter Your Book Name" />
+            <input
+              type="text"
+              placeholder="Enter Your Book Name"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyPress={searchBook}
+            />
             <button className="search-btn">
               {" "}
               <FaSearch />{" "}
@@ -27,16 +48,7 @@ const BookFinder = () => {
           </div>
         </div>
       </div>
-      <div className="container">
-        <CardPage></CardPage>
-        <CardPage></CardPage>
-        <CardPage></CardPage>
-        <CardPage></CardPage>
-        <CardPage></CardPage>
-        <CardPage></CardPage>
-        <CardPage></CardPage>
-        <CardPage></CardPage>
-      </div>
+      <div className="container">{<CardPage book={bookData} />}</div>
     </>
   );
 };
--- src/components/cardpage/card.jsx
+++ src/components/cardpage/card.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 
-const CardPage = () => {
+const CardPage = ({ book }) => {
+  console.log(book);
   return (
-    <div className="card">
-      <div>
-        <img src="public/image/book.jpg" alt="" />
-      </div>
-      <div className="bottom">
-        <h3 className="title">React JS</h3>
-        <p className="amount">{"\u20B9"} 3250</p>
-      </div>
-    </div>
+    <>
+      {book.map((item, index) => {
+        let thumbnail =
+          item.volumeInfo.imageLinks &&
+          item.volumeInfo.imageLinks.smallThumbnail;
+        thumbnail = !thumbnail
+          ? "https://pics.craiyon.com/2023-07-04/7ef326780d064781a329cc1f7dd9a2b3.webp"
+          : thumbnail;
+        console.log(item.saleInfo.listPrice);
+
+        return (
+          <div className="card" key={index}>
+            <div>
+              <img src={thumbnail} alt="img_not_found" />
+            </div>
+            <div className="bottom">
+              <h3 className="title">{item.volumeInfo.title} </h3>
+              <p className="amount">
+                {}
+                {item.saleInfo.listPrice === undefined
+                  ? "Not for sales"
+                  : "\u20B9 " + item.saleInfo.listPrice.amount}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
